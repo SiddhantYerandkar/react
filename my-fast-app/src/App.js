877dba709abb4af8c9e9ef4a062bd4603032d358
SiddhantYerandkar <siddhantyerandkar@gmail.com>
2023-01-04 23:32:31 +0530

    done

---
+++ my-fast-app/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      showParagraph: false,
+    };
+  }
+  render() {
+    console.log(this.state.input);
+    return (
+      <div>
+        <textarea
+          value={this.state.input}
+          onChange={(e) => this.setState({ input: e.target.value })}
+        />
+        <br />
+        <button
+          onClick={() => {
+            this.setState({
+              showParagraph: !this.state.showParagraph,
+            });
+          }}
+        >
+          Display in uppercase
+        </button>
+        {this.state.showParagraph && <p>{this.state.input.toUpperCase()}</p>}
+      </div>
+    );
+  }
+}
+
+export default App;
